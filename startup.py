d00ebffdf62239d95d02718225c542e9fa7c6ca6
alexbabij <alexbabij3@gmail.com>
2023-04-10 02:07:57 -0700

    added more checks etc.

--- startup.py
+++ startup.py
@@ -2,7 +2,9 @@ import serial, time
 port = "/dev/ttyGSM1"
 ser = serial.Serial(port, baudrate = 115200, timeout = 1) #make the timeout pretty big because it takes a second for it to open the serial channel I think
 PAUSE = 0.1
-print("Start")
+print("Startup\n")
+
+servStateDict = {'0': 'not known or not detectable', '1': 'radio off', '2': 'searching', '3': 'no service', '4': 'registered', '5': "string parsing or other error"}
 def sendCheck(command):
     command = command + "\r\n" #\r is carriage return which I think signals the end of the command
     ser.write(command.encode()) #.write sends the command over the serial port "ser"
@@ -14,30 +16,44 @@ def sendCheck(command):
     with open("startup_log.txt", "w") as f: #save our output to a log file
         f.write(repr(respString))
     startPos = respString.find("+UCGED: ") #starting position of response string (read_until doesnt clear the serial buffer, so running it twice in a row will detect our input twice)
-    servState = respString[startPos+13]
+    
+    if len(respString) > startPos+13: #If the string is too short i.e. from returning no value or "ERROR", we just say it has no connection with 0, instead of the script erroring out
+        servState = respString[startPos+13]
+    else:
+        servState = '0'
+    
+    if servState not in servStateDict: #If its still brokey we just say so 
+        servState = '5'
+
     time.sleep(PAUSE) # basically wait to send the next command
     return servState
 
 
+
+
+#Starting agps setup, first, checking to wait for a valid cellular connection 
 checkResult = '0'
 checkTime = 0
 checkTime5 = 6
 
 while checkResult != '4':
 
-    print(f"Waiting for cellular connection ({checkTime:.1f}s)", end="\r")
+    print("Waiting for cellular connection. Status:" , servStateDict[checkResult], f"({checkTime:.1f}s)", end="\r")
     
     if checkTime5 >= 7:
         checkResult = sendCheck("AT+UCGED?")
-        checkTime += PAUSE*2 #running the function adds time (by design) so we actually need to include this
+        checkTime += PAUSE #running the function adds time (by design) so we actually need to include this
         checkTime5 = 0 
     
     checkTime5 += 1
     checkTime += 1
     time.sleep(1.0)
 
-    if checkTime >= 120:
+    if checkTime >= 240:
         print("Timeout no connection")
         break
     
-print("\nReady")
+print("\nReady, running assisted GPS setup")
+
+
+#Need to make a second function to just send commands since sendCheck is its own thing
