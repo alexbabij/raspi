0fbcea6dc8dbd541e06d510bde21eccb027807f8
alexbabij <alexbabij3@gmail.com>
2023-04-10 19:34:34 -0700

    a

--- testing.py
+++ testing.py
@@ -8,22 +8,27 @@ time.sleep(1.0)
 GPSParams = '1,4,71'
 print("\nTurning on GPS with AT+UGPS="+GPSParams)
 
-def sendCommand(command):
+def sendCommand(command,timeout=1): #optional function input for timeout
     command = command + "\r\n"
     ser.write(command.encode())
-    output = ser.read_until()   # default is \n
+    output = ser.read_until('\n')   # default is \n
     print("Command sent:", output.rstrip().decode())     #rstrip will remove any trailing new lines or carriage return, this makes the output more readable
-    response = ser.read_until()
+    response = ser.read_until('\n',timeout)
     #response = ser.read(80)
     print("response", response.decode())
     time.sleep(PAUSE)
     return response
 
-sendCommand('AT+UGPS='+GPSParams)
-time.sleep(1.0)
+sendCommand('AT+UGPS='+GPSParams,timeout=3)
+time.sleep(5.0)
 gpsPResp = sendCommand('AT+UGPS?')
 gpsPResp = gpsPResp.rstrip()
-print(gpsPResp.decode()[7:])
 #Check if the gps is turned on with our parameters
-if gpsPResp.decode()[7:] != GPSParams: #there is no "7:end" in python, just leave this blank
+if gpsPResp.decode()[7:] == GPSParams: #there is no "7:end" in python, just leave this blank
+    print("Success")
+else:
     print("\nGPS setup failed, returned configuration of:", gpsPResp.decode())
+
+time.sleep(3.0)
+#Enable communication betweenn GPS and GSM by turning on unsolicited aiding 
+sendCommand("AT+UGIND=1")
