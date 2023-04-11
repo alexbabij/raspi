0fc113c2364f739eab9aee890b5c94aa1f87aa74
alexbabij <alexbabij3@gmail.com>
2023-04-10 19:34:01 -0700

    a

--- startup.py
+++ startup.py
@@ -73,19 +73,19 @@ time.sleep(1.0)
 GPSParams = '1,4,71'
 print("\nTurning on GPS with AT+UGPS="+GPSParams)
 
-def sendCommand(command): #optional function input for timeout
+def sendCommand(command,timeout=1): #optional function input for timeout
     command = command + "\r\n"
     ser.write(command.encode())
-    output = ser.read_until()   # default is \n
+    output = ser.read_until('\n')   # default is \n
     print("Command sent:", output.rstrip().decode())     #rstrip will remove any trailing new lines or carriage return, this makes the output more readable
-    response = ser.read_until()
+    response = ser.read_until('\n',timeout)
     #response = ser.read(80)
     print("response", response.decode())
     time.sleep(PAUSE)
     return response
 
-sendCommand('AT+UGPS='+GPSParams)
-time.sleep(10.0)
+sendCommand('AT+UGPS='+GPSParams,timeout=3)
+time.sleep(5.0)
 gpsPResp = sendCommand('AT+UGPS?')
 gpsPResp = gpsPResp.rstrip()
 #Check if the gps is turned on with our parameters
