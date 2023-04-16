d2287d419401bebff50d7a51a1ab042b34ef4929
alexbabij <alexbabij3@gmail.com>
2023-04-16 01:02:19 -0700

    a

--- test_display.py
+++ test_display.py
@@ -3,13 +3,17 @@
 import time
 from display_text import *
 
+timee = 0
 counter = 0
+startt = time.time()
 while True:
 
     counter += 1
-    string = "test,"+str(counter)
+    timee += 0.1
+    truet = time.time()-startt
+    string = "test,"+str(round(timee,1))+" "+str(round(truet),2)
     dispText(string,counter*10)
-    time.sleep(0.5)
+    time.sleep(0.1)
     if counter >= 24:
         counter = 0
 
