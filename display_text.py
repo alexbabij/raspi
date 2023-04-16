94c928dae51b1c52eb253c012ff71ec8d9f5919a
alexbabij <alexbabij3@gmail.com>
2023-04-16 00:52:48 -0700

    reduce unnessecary updates

--- display_text.py
+++ display_text.py
@@ -6,35 +6,33 @@ from PIL import Image, ImageDraw, ImageFont
 
 from adafruit_rgb_display import st7735  # pylint: disable=unused-import
 
-def dispText(textIn,textLoc,FONTSIZE=15):
+# Configuration for CS and DC pins (these are PiTFT defaults):
+cs_pin = digitalio.DigitalInOut(board.CE0)
+dc_pin = digitalio.DigitalInOut(board.D25)
+reset_pin = digitalio.DigitalInOut(board.D24)
+
+# Config for display baudrate (default max is 24mhz):
+BAUDRATE = 24000000
+
+# Setup SPI bus using hardware SPI:
+spi = board.SPI()
+
+
+disp = st7735.ST7735R(spi, rotation=90, cs=cs_pin, dc=dc_pin, rst=reset_pin, baudrate=BAUDRATE)
+# Create blank image for drawing.
+# Make sure to create image with mode 'RGB' for full color.
+if disp.rotation % 180 == 90:
+    height = disp.width  # we swap height/width to rotate it to landscape!
+    width = disp.height
+else:
+    width = disp.width  # we swap height/width to rotate it to landscape!
+    height = disp.height
+
+def dispText(textIn,textLoc,FONTSIZE=15,width=width,height=height):
     startTime = time.time()
     # First define some constants to allow easy resizing of shapes.
     BORDER = 20
     #FONTSIZE = 20
-
-    # Configuration for CS and DC pins (these are PiTFT defaults):
-    cs_pin = digitalio.DigitalInOut(board.CE0)
-    dc_pin = digitalio.DigitalInOut(board.D25)
-    reset_pin = digitalio.DigitalInOut(board.D24)
-
-    # Config for display baudrate (default max is 24mhz):
-    BAUDRATE = 24000000
-
-    # Setup SPI bus using hardware SPI:
-    spi = board.SPI()
-
-
-    disp = st7735.ST7735R(spi, rotation=90, cs=cs_pin, dc=dc_pin, rst=reset_pin, baudrate=BAUDRATE)
-
-
-    # Create blank image for drawing.
-    # Make sure to create image with mode 'RGB' for full color.
-    if disp.rotation % 180 == 90:
-        height = disp.width  # we swap height/width to rotate it to landscape!
-        width = disp.height
-    else:
-        width = disp.width  # we swap height/width to rotate it to landscape!
-        height = disp.height
 
     image = Image.new("RGB", (width, height))
 
