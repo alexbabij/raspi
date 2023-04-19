53b976cb07a18ba8c5f667153522638aa916e766
alexbabij <alexbabij3@gmail.com>
2023-04-18 21:44:55 -0700

    a

--- python_gps_ozzm/python-measure-G/ozzmaker-LTE-IMU-measure-G.py
+++ python_gps_ozzm/python-measure-G/ozzmaker-LTE-IMU-measure-G.py
@@ -13,11 +13,40 @@ import time
 import IMU
 import sys
 import datetime
+gyroXangle = 0.0
+gyroYangle = 0.0
+gyroZangle = 0.0
+CFangleX = 0.0
+CFangleY = 0.0
+CFangleXFiltered = 0.0
+CFangleYFiltered = 0.0
+kalmanX = 0.0
+kalmanY = 0.0
+oldXMagRawValue = 0
+oldYMagRawValue = 0
+oldZMagRawValue = 0
+oldXAccRawValue = 0
+oldYAccRawValue = 0
+oldZAccRawValue = 0
 
+RAD_TO_DEG = 57.29578
+M_PI = 3.14159265358979323846
+G_GAIN = 0.070          # [deg/s/LSB]  If you change the dps for gyro, you need to update this value accordingly
+AA =  0.40              # Complementary filter constant
+MAG_LPF_FACTOR = 0.4    # Low pass filter constant magnetometer
+ACC_LPF_FACTOR = 0.4    # Low pass filter constant for accelerometer
+ACC_MEDIANTABLESIZE = 9         # Median filter table size for accelerometer. Higher = smoother but a longer delay
+MAG_MEDIANTABLESIZE = 9         # Median filter table size for magnetometer. Higher = smoother but a longer delay
 
 IMU.detectIMU()     #Detect if BerryIMU is connected.
 IMU.initIMU()       #Initialise the accelerometer, gyroscope and compass
-
+#calibration data
+magXmin =  122893
+magYmin =  123672
+magZmin =  123152
+magXmax =  138807
+magYmax =  139808
+magZmax =  140640
 
 fGrav = 9.80674 #m/s^2 Force of gravity in Spokane, WA
 a = datetime.datetime.now()
@@ -38,6 +67,18 @@ while True:
     MAGx = IMU.readMAGx() 
     MAGy = IMU.readMAGy() 
     MAGz = IMU.readMAGz() 
+    #Apply compass calibration
+    MAGx -= (magXmin + magXmax) /2
+    MAGy -= (magYmin + magYmax) /2
+    MAGz -= (magZmin + magZmax) /2
+
+    MAGx =  MAGx  * MAG_LPF_FACTOR + oldXMagRawValue*(1 - MAG_LPF_FACTOR);
+    MAGy =  MAGy  * MAG_LPF_FACTOR + oldYMagRawValue*(1 - MAG_LPF_FACTOR);
+    MAGz =  MAGz  * MAG_LPF_FACTOR + oldZMagRawValue*(1 - MAG_LPF_FACTOR);
+    ACCx =  ACCx  * ACC_LPF_FACTOR + oldXAccRawValue*(1 - ACC_LPF_FACTOR);
+    ACCy =  ACCy  * ACC_LPF_FACTOR + oldYAccRawValue*(1 - ACC_LPF_FACTOR);
+    ACCz =  ACCz  * ACC_LPF_FACTOR + oldZAccRawValue*(1 - ACC_LPF_FACTOR);
+
     MAGx  = MAGx * (1.0/16384.0)  #18 bits
     MAGy  = MAGy * (1.0/16384.0)  #18 bits
     MAGz  = MAGz * (1.0/16384.0)  #18 bits
