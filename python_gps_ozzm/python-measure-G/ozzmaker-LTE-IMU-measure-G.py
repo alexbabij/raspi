a7abd22356c2f02cb4f18b77eaece23a9ae6ca70
alexbabij <alexbabij3@gmail.com>
2023-04-18 21:04:11 -0700

    a

--- python_gps_ozzm/python-measure-G/ozzmaker-LTE-IMU-measure-G.py
+++ python_gps_ozzm/python-measure-G/ozzmaker-LTE-IMU-measure-G.py
@@ -12,7 +12,7 @@
 import time
 import IMU
 import sys
-
+import datetime
 
 
 IMU.detectIMU()     #Detect if BerryIMU is connected.
@@ -20,9 +20,16 @@ IMU.initIMU()       #Initialise the accelerometer, gyroscope and compass
 
 
 fGrav = 9.80674 #m/s^2 Force of gravity in Spokane, WA
+a = datetime.datetime.now()
 
 while True:
-
+ 
+ ##Calculate loop Period(LP). How long between Gyro Reads
+    b = datetime.datetime.now() - a
+    a = datetime.datetime.now()
+    LP = b.microseconds/(1000000*1.0)
+    outputString = "Loop Time %5.5f " % ( LP )
+    print(outputString)
 
     #Read the accelerometer,gyroscope and magnetometer values, once being turned into values, is measured in G's, convert to m/s: 
     ACCx = IMU.readACCx()
@@ -36,4 +43,4 @@ while True:
 
 
     #slow program down a bit, makes the output more readable
-    time.sleep(0.03)
+    #time.sleep(0.03)
