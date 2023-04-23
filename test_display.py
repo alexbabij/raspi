6dd16e0614034ce7165929288fdeb5f970835d69
alexbabij <alexbabij3@gmail.com>
2023-04-22 22:37:32 -0700

    a

--- test_display.py
+++ test_display.py
@@ -8,16 +8,19 @@ counter = 0
 startt = time.time()
 rounding = 0
 location = 0
+frameC = 0
 loDict = {1: "northwest", 2: "southwest", 3: "southeast", 4: "northeast"}
 dispBackground([0,0,0])
 while True:
 
+    frameC += 1
     counter += 1
     timee += 0.1
     rounding += 1
     location += 1
     truet = time.time()-startt
-    string = "test,"+str(round(timee,1))+"\n"+str(round(truet,rounding))
+    string = "test,"+str(round(timee,1))+"\n"+str(round(truet,rounding),
+        "\nFrame:",frameC)
     dispText(string,loDict[location],fontColor=[205,153,51,255])
     time.sleep(0.1)
     if counter >= 24:
