411de40de437db059fe0e457a0caed7cdfaa3932
alexbabij <alexbabij3@gmail.com>
2023-04-23 23:49:19 -0700

    a

--- send_commands.py
+++ send_commands.py
@@ -20,9 +20,12 @@ def sendCommand(command,readall=False): #optional function input for timeout
     return response
 
 
-
+##portable:
 #sendCommand("AT+UGUBX=\"B5 62 06 24 24 00 FF FF 00 02 00 00 00 00 10 27 00 00 05 00 FA 00 FA 00 64 00 2C 01 00 00 00 00 10 27 00 00 00 00 00 00 00 00 46 EE\"")
-sendCommand("AT+UGUBX=\"B5 62 06 08 06 00 F4 01 01 00 01 00 0B 77\"")
+#sendCommand("AT+UGUBX=\"B5 62 06 08 06 00 F4 01 01 00 01 00 0B 77\"")
+#STationary 
+print("sending command")
+sendCommand("AT+UGUBX=\"B5 62 06 24 24 00 FF FF 02 03 00 00 00 00 10 27 00 00 05 00 FA 00 FA 00 64 00 2C 01 00 00 00 00 10 27 00 00 00 00 00 00 00 00 49 53\"")
 
 
 
