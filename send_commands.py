eda839e170fd89f09185c6f3c416fb2899729a38
alexbabij <alexbabij3@gmail.com>
2023-04-25 01:52:34 -0700

    a

--- send_commands.py
+++ send_commands.py
@@ -33,15 +33,16 @@ print("sending command")
 #automotive
 #sendCommand("AT+UGUBX=\"B5 62 06 24 24 00 FF FF 04 03 00 00 00 00 10 27 00 00 05 00 FA 00 FA 00 64 00 2C 01 00 00 00 00 10 27 00 00 00 00 00 00 00 00 4B 97\"")
 start = time.time()
-sendCommand("AT+UGUBX=\"B5 62 06 24 24 00 FF FF 00 02 00 00 00 00 10 27 00 00 05 00 FA 00 FA 00 64 00 2C 01 00 00 00 00 10 27 00 00 00 00 00 00 00 00 46 EE\"")
-print('start 1st command send')
-# #set update rate 4Hz
-sendCommand("AT+UGUBX=\"B5 62 06 08 06 00 FA 00 01 00 01 00 10 96\"")
-print('end 1st send')
-time.sleep(1.0)
-#set update rate 2Hz
-sendCommand("AT+UGUBX=\"B5 62 06 08 06 00 F4 01 01 00 01 00 0B 77\"")
-#portable 2d only
-sendCommand("AT+UGUBX=\"B5 62 06 24 24 00 FF FF 00 01 00 00 00 00 10 27 00 00 05 00 FA 00 FA 00 64 00 2C 01 00 00 00 00 10 27 00 00 00 00 00 00 00 00 45 CD\"")
+# sendCommand("AT+UGUBX=\"B5 62 06 24 24 00 FF FF 00 02 00 00 00 00 10 27 00 00 05 00 FA 00 FA 00 64 00 2C 01 00 00 00 00 10 27 00 00 00 00 00 00 00 00 46 EE\"")
+# print('start 1st command send')
+# # #set update rate 4Hz
+# sendCommand("AT+UGUBX=\"B5 62 06 08 06 00 FA 00 01 00 01 00 10 96\"")
+# print('end 1st send')
+# time.sleep(1.0)
+# #set update rate 2Hz
+# sendCommand("AT+UGUBX=\"B5 62 06 08 06 00 F4 01 01 00 01 00 0B 77\"")
+# #portable 2d only
+# sendCommand("AT+UGUBX=\"B5 62 06 24 24 00 FF FF 00 01 00 00 00 00 10 27 00 00 05 00 FA 00 FA 00 64 00 2C 01 00 00 00 00 10 27 00 00 00 00 00 00 00 00 45 CD\"")
+sendCommand("AT+UGUBX=\"b56206040400ffff02000e61\"")
 print('elapsed time', time.time()-start)
 print("\nEnd Setup")
