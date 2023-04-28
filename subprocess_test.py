14f25b855b21958e654922fd850778567641422d
alexbabij <alexbabij3@gmail.com>
2023-04-28 01:52:47 -0700

    a

--- subprocess_test.py
+++ subprocess_test.py
@@ -1,5 +1,5 @@
 import subprocess
-subprocess.Popen(["./run_cgps.sh"],start_new_session=True)
-subprocess.Popen(["python","collect_gpsdata.py"],start_new_session=True)
+subprocess.Popen(["./run_cgps.sh"])
+subprocess.Popen(["python","collect_gpsdata.py"])
 #This is the workaround for one of the most annoying and impossible to troubleshoot issues out there that I won't even get into
 print("\nEnd Setup")
