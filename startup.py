c366d6a4380c78014a370360d16777505c8fe0d2
alexbabij <alexbabij3@gmail.com>
2023-04-28 02:44:27 -0700

    a

--- startup.py
+++ startup.py
@@ -152,6 +152,6 @@ else:
 # sendCommand("AT+UGUBX=\""+modelDict['automotive']+"\"")
 
 subprocess.Popen(["./run_cgps.sh"],start_new_session=True)
-subprocess.Popen(["python","collect_gpsdata.py"],start_new_session=True)
+#subprocess.Popen(["python","collect_gpsdata.py"],start_new_session=True)
 #This is the workaround for one of the most annoying and impossible to troubleshoot issues out there that I won't even get into
 print("\nEnd Setup")
