5efe1fedab63c70d5ea695b17f16142501b5470c
alexbabij <alexbabij3@gmail.com>
2023-05-04 21:12:00 -0700

    maybe not idk

--- runcgpspython.py
+++ runcgpspython.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python
+
 import subprocess
 #This runs cgps in a separate python script that autoruns so it doesnt put stuff in the terminal window
 #We can also change this through github since its in raspi/
 print('running cpgs :(')
-subprocess.Popen(["./run_cgps.sh"],start_new_session=True)
+subprocess.Popen(["./run_cgps.sh"],start_new_session=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
